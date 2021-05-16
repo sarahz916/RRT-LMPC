7fbceb852f67a66a74a7794f4947985c5ea0834a
sarahz916 <53065564+sarahz916@users.noreply.github.com>
2021-05-15 23:04:20 -0600

    adding basic body code

---
+++ body.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 21:25:54 2021
+
+@author: zousa
+"""
+# Body Class for CS 159 Final Project
+
+import numpy as np
+
+class Body():
+    def __init__(self, obs_list: list, start: tuple, end: tuple, max_grid: tuple ):
+        """
+        
+        Parameters
+        ----------
+        obs_list : list
+            List of obstacles
+        start : tuple
+            Start coordinate
+        end : tuple
+            End coordinate
+        max_grid : tuple
+            Maximium x value and Maximum y value of workspace
+
+        Returns
+        -------
+        None.
+
+        """
+        self.obs_list = obs_list
+        self.start = start
+        self.end = end
+        self.max_x = max_grid[0]
+        self.max_y = max_grid[1]
---
+++ rrt.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 21:25:08 2021
+
+@author: zousa
+"""
+# RRT Code for CS 159 Final Project
+
