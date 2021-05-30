Fixed ftocp and explicit jacobian

ftocpLMPC.py
@@ -125,9 +125,9 @@ class FTOCP(object):
     # Aaron: modify the uGuess using the past trajectory so that can repeat and
     # solve via SQP
     def uGuessUpdate(self):
-        uPred = self.uPred
+        # uPred = self.uPred
         for i in range(0, self.N):
-            self.uGuess[i] = uPred[i]
+            self.uGuess[i] = np.copy(self.uPred[i])
         
     # Potentially round s-values that are negative but close to 0 to 0 and 
     # similarly for v-values
@@ -135,7 +135,8 @@ class FTOCP(object):
         # Extract predicted state and predicted input trajectories
         self.xPred = np.vstack((x0, np.reshape((self.Solution[np.arange(self.n*(self.N))]),(self.N,self.n))))
         self.uPred = np.reshape((self.Solution[self.n*(self.N)+np.arange(self.d*self.N)]),(self.N, self.d))
-        
+        self.lambdas = self.Solution[self.n*self.N+self.d*self.N:]
+                
         if roundIt:
             print('Called roundIt')
             self.xPred[np.where((self.xPred[:,0] < 0) * (np.abs(self.xPred[:,0]) < 1e-5)),0] = 0 
@@ -233,7 +234,7 @@ class FTOCP(object):
         # Hint 1: The equality constraint is: [Gx, Gu]*z = E * x(t) + C
         # Hint 2: Write on paper the matrices Gx and Gu to see how these matrices are constructed
         Gx = np.eye(self.n * self.N )
-        Gu = np.zeros((self.n * self.N, self.d*self.N) )
+        Gu = np.zeros((self.n * self.N, self.d*self.N))
 
         self.C = []
         E_eq = np.zeros((Gx.shape[0], self.n))
@@ -251,11 +252,14 @@ class FTOCP(object):
         G_eq = np.hstack((Gx, Gu))
         C_eq = self.C
         
+        # G_eq z = E_eq x0 + C_eq where z = [x | u | lambdas]
         # Modify G_eq to account for lambda
         largeG = np.zeros((G_eq.shape[0]+self.n+1, G_eq.shape[1]+self.k))
         largeG[:G_eq.shape[0], :G_eq.shape[1]] = G_eq
-        largeG[G_eq.shape[0] + 1:, self.n * (self.N-1): self.n * self.N] = -np.eye(self.n)
-        largeG[G_eq.shape[0] + 1:, -self.k:] = self.terminalPoints
+        # [0 ... 0 -I | 0 ... 0 | P] [x | u | lambdas].T = -I x_N + P lambdas
+        largeG[G_eq.shape[0]:(G_eq.shape[0]+self.n), self.n * (self.N-1): self.n * self.N] = -np.eye(self.n)
+        largeG[G_eq.shape[0]:(G_eq.shape[0]+self.n), -self.k:] = self.terminalPoints
+        # [0...0 | 0...0 | 1 ... 1] [x | u | lambdas].T = sum(lambdas)
         largeG[-1, -self.k:] = 1 # Adds constraint that sum of lambdas = 1
         G_eq = largeG
         
@@ -263,7 +267,9 @@ class FTOCP(object):
         E_eq = np.vstack([E_eq, np.zeros((self.n + 1, E_eq.shape[1]))])
         #C_eq = np.vstack([C_eq.T, np.zeros((self.k,))]) #C_eq.shape is (4, ) 
         C_eq = np.concatenate((C_eq, np.zeros((self.n + 1,))), axis=0)
-                
+        # Make last element 1 so that lambdas sum to 1        
+        C_eq[-1] = 1
+        
         if self.printLevel >= 2:
             print("G_eq: ")
             print(G_eq.shape)
@@ -309,7 +315,7 @@ class FTOCP(object):
 
     #     return A_linearized, B_linearized, C_linearized
 
-    def buildLinearizedMatrices(self, x, u):
+    def buildLinearizedMatrices(self, x, u, analytical=True):
         # s = x[0]
         # y = x[1]
         # v = x[2]
@@ -357,15 +363,69 @@ class FTOCP(object):
         # C = self.dynamics(x, u) - A @ x - B @ u
             
         # return A, B, C
-                                
-        merged = np.array(list(x) + list(u))
-        numJ = approx_jacobian(merged, lambda x: self.dynamics(x[:4], x[4:]), _epsilon)
-        
-        numA = numJ[:,:4]
-        numB = numJ[:, 4:]
-        numC = self.dynamics(x, u) - numA @ x - numB @ u
+                
+        if analytical:
+            spline = self.spline
+            dt = self.dt
+                            
+            s = x[0]
+            y = x[1]
+            v = x[2]
+            theta = x[3]
+            gamma = spline.calc_yaw(s)
+            k = spline.calc_curvature(s)
+            kPrime = spline.calc_curvaturePrime(s)
+            gammaPrime = spline.calc_yawPrime(s)
+            # d/ds [ cos(theta - gamma) / (1- gamma K)]
+            num = sin(theta - gamma) * gammaPrime * (1 - y * k) + \
+                    cos(theta - gamma) * y * kPrime
+            den = (1 - y * k)**2
             
-        return numA, numB, numC
+            A = np.zeros((4,4))
+            
+            # s derivatives
+            A[0,0] = 1 + v * dt * num/den
+            A[1,0] = - v * dt * cos(theta - gamma) * gammaPrime 
+            A[2,0] = 0
+            A[3,0] = 0
+            
+            # y derivatives
+            A[0,1] = v * dt * cos(theta - gamma) * k / den
+            A[1,1] = 1
+            A[2,1] = 0
+            A[3,1] = 0
+            
+            # v derivatives
+            A[0,2] = dt * cos(theta - gamma) / (1- y * k)
+            A[1,2] = dt * sin(theta - gamma)
+            A[2,2] = 1
+            A[3,2] = 0
+            
+            # theta derivatives
+            A[0,3] = - dt * v * sin(theta - gamma) / (1 - y * k)
+            A[1,3] = dt * v * cos(theta - gamma)
+            A[2,3] = 0
+            A[3,3] = 1
+            
+            B = np.zeros((4,2))
+            
+            B[2,0] = dt
+            B[3,1] = dt
+                
+            C = self.dynamics(x, u) - A @ x - B @ u
+            
+            return A, B, C
+
+        if not analytical:
+            merged = np.array(list(x) + list(u))
+            numJ = approx_jacobian(merged, lambda x: self.dynamics(x[:4], x[4:]), _epsilon)
+            
+            numA = numJ[:,:4]
+            numB = numJ[:, 4:]
+            numC = self.dynamics(x, u) - numA @ x - numB @ u
+                
+            return numA, numB, numC
+        
     
     def osqp_solve_qp(self, P, q, G= None, h=None, A=None, b=None, initvals=None):
         """ 

lmpc.py
@@ -46,7 +46,7 @@ class LMPC(object):
     # x0 is the start state, goal is the target state
     # Ultimately, when are repeatedly calling this function can use previous
     # uPred with offset of one as uGuess
-    def runSQP(self, x0, goal, terminalPoints, valuePoints, uGuess = None, numIters=2):
+    def runSQP(self, x0, goal, terminalPoints, valuePoints, uGuess = None, numIters=5):
         if uGuess is None:
             uGuess = [np.array([self.amax / 100, 0])]*self.N
         
@@ -57,12 +57,11 @@ class LMPC(object):
             ax.set_xlabel('$x_1$')
             ax.set_ylabel('$x_2$')
                 
-        # TODO: TEMPORARY
-        # SHOULD PUT BACK TO using ftocp.xPred instead of xDemo
         # Let's see what would happen if used uGuess to propogate solution
         xDemo = [x0]
         for i, ut in enumerate(uGuess):
             xDemo.append(self.dynamics(xDemo[-1], ut))
+        demoXY = self.convertToXY(xDemo)
         
         for i in range(numIters):
             ftocp.solve(x0)
@@ -88,24 +87,40 @@ class LMPC(object):
                 
                 xTrue = [x0]
                 for j, ut in enumerate(ftocp.uPred):
-                    xDemo.append(self.dynamics(xTrue[-1], ut))
+                    xTrue.append(self.dynamics(xTrue[-1], ut))
+                trueXY = self.convertToXY(xTrue)
                 
-                if i == 0:
-                    color='r'
-                else:
-                    color='b'
+                # Propogate the dynamics using the linear approximation and
+                # see what happens
+                xProp = [x0]
+                for j, ut in enumerate(ftocp.uPred):
+                    xBar = ftocp.xGuess[j]
+                    uBar = ftocp.uGuess[j]
+                    A, B, C = ftocp.buildLinearizedMatrices(xBar, uBar)
+                    xNext = A @ xProp[-1] + B @ ut + C
+                    xProp.append(xNext)
+                
+                xyProp = self.convertToXY(xProp)
+                
                 print('i = ' + str(i))
-                ax.plot(xyCoords[:,0], xyCoords[:,1], '--o'+color, label='SQP iter = ' + str(i))
-                
+                # ax.plot(xyCoords[:,0], xyCoords[:,1], '--o', label='FTOCP result ' + str(i))
+                ax.plot(trueXY[:,0], trueXY[:,1], '--o', label='Nonlinear result ' + str(i))
+                # ax.plot(xyProp[:,0], xyProp[:,1], '--o', label='Linear result ' + str(i))
                 # Visualize the safe set
                 if i == 0: 
                     terminalXY = self.convertToXY(terminalPoints.T)
-                    ax.scatter(terminalXY[:,0], terminalXY[:,1], s=50, label='Terminal Points')
+                    ax.scatter(terminalXY[:,0], terminalXY[:,1], color='k', s=50, label='Terminal Points')
+                    ax.plot(demoXY[:,0], demoXY[:,1], '--o', label='Guess ' + str(i))
                 ax.legend()
                 plt.pause(0.001)
                 
-                pdb.set_trace()
+                print('Lambdas sum to: ', sum(ftocp.lambdas))
+                print('Specify combination: ', sum([ftocp.lambdas[i] * ftocp.terminalPoints.T[i] for i in range(30)]))
+                print('ftocp final: ', ftocp.xPred[-1])
             
+            if not ftocp.feasible or None in ftocp.xPred:
+                pdb.set_trace()
+                
             # To update the linearization, use as uGuess the uPred from
             # previous iteration (will then also update xGuess internally)
             ftocp.uGuessUpdate() 
@@ -123,9 +138,12 @@ class LMPC(object):
         uPred = []
         
         count = 0
-        x0 = self.start
-        xTraj = []
+        xTraj = [self.start]
         uTraj = []
+        
+        if self.printLevel >= 1:
+            plt.ion()
+            fig, ax = plt.subplots()
         
         # while not sufficiently close to goal state and < max iterations:
         while distLeft > eps and count < maxIter:
@@ -152,7 +170,11 @@ class LMPC(object):
             terminalPoints = np.array([self.SS[ind] for ind in safeIndices]).T
             valuePoints = np.array([self.values[ind] for ind in safeIndices])
             
-            if self.printLevel >= 2:
+            if self.printLevel >= 1:
+                x_1, y_1 = self.spline.calcXY(*xTraj[-1][:2])
+                ax.scatter(x_1, y_1, color='r')
+                
+            if self.printLevel >= 1 and count % 10 == 0:
                 # Visualize the safe set and target x,y
                 terminalXY = self.convertToXY(terminalPoints.T)
                 targetXY = self.spline.calcXY(target[0], target[1])
@@ -160,17 +182,20 @@ class LMPC(object):
                 # Visualize the full set of demo points
                 demoXY = self.convertToXY(xDemo)
                 
-                plt.figure()
-                plt.scatter(demoXY[:,0], demoXY[:,1], label='Full Demo')
-                plt.scatter(terminalXY[:,0], terminalXY[:,1], label='Terminal Points')
-                plt.scatter(targetXY[0], targetXY[1], label='Target')
+                ax.scatter(terminalXY[:,0], terminalXY[:,1], label='Terminal Points')
+                ax.scatter(targetXY[0], targetXY[1], label='Target')
+                
+                # plt.figure()
+                # plt.scatter(demoXY[:,0], demoXY[:,1], label='Full Demo')
+                # plt.scatter(terminalXY[:,0], terminalXY[:,1], label='Terminal Points')
+                # plt.scatter(targetXY[0], targetXY[1], label='Target')
                 
                 plt.legend()
-                plt.title('Visualizing Safe Set Points')
-                plt.xlabel(r'$x_1$')
-                plt.ylabel(r'$x_2$')
-                plt.xlim([0,20])
-                plt.ylim([0,20])
+                # plt.title('Visualizing Safe Set Points')
+                # plt.xlabel(r'$x_1$')
+                # plt.ylabel(r'$x_2$')
+                # plt.xlim([0,20])
+                # plt.ylim([0,20])
                 
             # 3. If this is not the first iteration, set uGuess using
             # one-offset from past SQP uPred as in HW2 problem 1 ftocp 
@@ -186,12 +211,12 @@ class LMPC(object):
             
             # 4. runSQP using the current state. Save uPred for step 3 and 
             # last state in xPred for step 1.
-            xPred, uPred = self.runSQP(x0, self.goal, terminalPoints, valuePoints, uGuess)
+            xPred, uPred = self.runSQP(xTraj[-1], self.goal, terminalPoints, valuePoints, uGuess)
             
             # 5. Execute the first control action:
             # compute the updated state using the *nonlinear* dynamics and this
             # control action.
-            xNext = self.dynamics(x0, uPred[0])
+            xNext = self.dynamics(xTraj[-1], uPred[0])
             
             # 6. Record the executed control action and updated state  
             xTraj.append(xNext)

lmpcMain.py
@@ -66,7 +66,7 @@ def main(j: int):
         
         # Create the spline
         path = np.array(path)
-        spline = Spline2D(path[:,0], path[:,1])
+        spline = Spline2D(path[:,0], path[:,1], ds=0.001)
                 
         # Need to create list of states for path
         fitted_path = fit_path(path, ds = 0.01)
@@ -81,7 +81,7 @@ def main(j: int):
     
     lmpcSolver = LMPC(N, K, Q, Qf, R, [], [], spline, dt, width, amax, amin, theta_dotMax, theta_dotMin, printLevel)    
     
-    plt.figure()
+    # plt.figure()
            
     # splineLine = []
     # for s in np.linspace(0, spline.end, 1000, endpoint=False):
@@ -152,9 +152,9 @@ def main(j: int):
             plt.figure()
             demoX = demo[1][1:]
             plt.plot(demoX[:,0], demoX[:,1], '--og', label='Original Demo')
-            # plt.plot(xyCoords[:,0], xyCoords[:,1], '--ob', label='Converted to S and Back')
-            # plt.plot(xExp[:,0], xExp[:,1], '--or', label='Predicted from control x')
-            # plt.plot(xSExp[:,0], xSExp[:,1], '--ok', label='Predicted from control s')
+            plt.plot(xyCoords[:,0], xyCoords[:,1], '--ob', label='Converted to S and Back')
+            plt.plot(xExp[:,0], xExp[:,1], '--or', label='Predicted from control x')
+            plt.plot(xSExp[:,0], xSExp[:,1], '--ok', label='Predicted from control s')
             plt.plot(splineLine[:,0], splineLine[:,1], '--oy', label='Spline')
             plt.legend()
             

verifyingLinearization.py
@@ -67,7 +67,7 @@ def dynamics(x, u, dt, spline):
         curvature = spline.calc_curvature(x[0])
     except:
         pdb.set_trace()
-    deltaS = x[2] * cos(x[3] - gamma) / (1 - gamma * curvature)
+    deltaS = x[2] * cos(x[3] - gamma) / (1 - x[1] * curvature)
     deltaY = x[2] * sin(x[3] - gamma)
     s_next      = x[0] + dt * deltaS
     y_next      = x[1] + dt * deltaY
@@ -95,9 +95,9 @@ def buildLinearizedMatrices(x, u, dt, spline):
     kPrime = spline.calc_curvaturePrime(s)
     gammaPrime = spline.calc_yawPrime(s)
     # d/ds [ cos(theta - gamma) / (1- gamma K)]
-    num = sin(theta - gamma) * gammaPrime * (1 - gamma * k) + \
-            cos(theta - gamma) * (gammaPrime * k + kPrime * gamma)
-    den = (1 - gamma * k)**2
+    num = sin(theta - gamma) * gammaPrime * (1 - y * k) + \
+            cos(theta - gamma) * y * kPrime
+    den = (1 - y * k)**2
     
     A = np.zeros((4,4))
     
@@ -108,19 +108,19 @@ def buildLinearizedMatrices(x, u, dt, spline):
     A[3,0] = 0
     
     # y derivatives
-    A[0,1] = 0
+    A[0,1] = v * dt * cos(theta - gamma) * k / den
     A[1,1] = 1
     A[2,1] = 0
     A[3,1] = 0
     
     # v derivatives
-    A[0,2] = dt * cos(theta - gamma) / (1-gamma * k)
+    A[0,2] = dt * cos(theta - gamma) / (1- y * k)
     A[1,2] = dt * sin(theta - gamma)
     A[2,2] = 1
     A[3,2] = 0
     
     # theta derivatives
-    A[0,3] = - dt * v * sin(theta - gamma) / (1 - gamma * k)
+    A[0,3] = - dt * v * sin(theta - gamma) / (1 - y * k)
     A[1,3] = dt * v * cos(theta - gamma)
     A[2,3] = 0
     A[3,3] = 1
@@ -152,7 +152,7 @@ if __name__ == '__main__':
           
     # alphaVals = [0.1 * i for i in range(10)]
     alphaVals = [0, 0.5, 1]
-    dtVals = [10**i for i in range(-2,2)]
+    dtVals = [10**i for i in range(-1,2)]
     
     meanErrors = np.zeros((len(alphaVals), len(dtVals), 4))
     